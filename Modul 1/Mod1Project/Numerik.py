451252b78d8150acaf5516c20307569a92f96fc1
vitoradityafauzan <vitoraditya.f@gmail.com>
2021-04-14 14:25:10 +0700

    Praktikum, Modul 1

---
+++ Modul 1/Mod1Project/Numerik.py
@@ -0,0 +1,31 @@
+'''
+BILANGAN BULAT
+'''
+## Biner
+a = 0b1001
+#a
+## Oktal
+b = 0o23
+#b
+## Heksadesimal
+c = 0x2f
+#c
+## Boolean
+d = True
+#type(d)
+#int(d)
+
+## Perhitungan Penambahan
+e = 15
+#id(e)
+#e+= 5
+#e
+#id(e)
+
+'''
+BILANGAN RIIL
+'''
+## Float / decimal
+f = 123.456
+#a
+#a * 2
